e2223b1b65d26b389833cdc4a71dcd99ce5f7cfa
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-05-31 10:26:47 -0700

    commit

--- DSA__/src/greedy/P2_Job_Sequencing_problem.java
+++ DSA__/src/greedy/P2_Job_Sequencing_problem.java
@@ -1,7 +1,8 @@
 package greedy;
 
+// in job sequening problems you will get solution.
 public class P2_Job_Sequencing_problem {
 public static void main(String args[]) {
-	
+	System.out.println("");
 }
 }
