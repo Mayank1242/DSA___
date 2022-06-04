a7476ced9995f157a8ee2e463945080f8ea75829
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-06-04 01:17:43 -0700

    commit

--- DSA__/src/graph/Graph15_Disjoint_set.java
+++ DSA__/src/graph/Graph15_Disjoint_set.java
@@ -1,5 +1,93 @@
 package graph;
 
 public class Graph15_Disjoint_set {
-
+//Disjoint set it is used in kurskuls algo 
+//it is used to checkmif there are too much compoent so the are from same or not..
+//it provude to opertaion find parent operation and union operation.
+/*
+ 
+example-->
+	if they are component like 1 2 3 4 5 6 7 they are all 7 different component.
+	
+	they are look like-
+	1   2  3
+	4   5  6
+	all are its parent.means 1's parent is 1. and 2's parent is 2.
+	
+	for do union of that it->
+--------------------------------------------------------------------
+	union(1,2)- 
+	(1  2) 3
+     4  5  6 7
+     find parent of 2 is now 1...
+     
+     union(2,3)-
+     (1  2 3)
+     4  5  6 7
+     so parent of 3 is now 1..
+ 	
+ 	union(4,5)-
+     (1  2 3)
+     (4  5)  6 7
+     so parent of 5 is now 4..
+     
+     union(6,7)-
+     (1  2 3)
+     (4  5) (6 7)
+     so parent of 6 is now 7..
+     
+          union(5,6)-
+     (1  2 3)
+     (4  5 6 7)
+     so parent of  7 is now 4..
+ ---------------------------------------------------------------------
+ for check they are belong from the same graph or not -Check there parent.
+ 
+  efficient implementation of disjoint set is by path compression and union by rank.   
+  
+ --------------------------------------------------------------------------
+ 
+ 1- we have a parent array and every node is himself its  parent array.
+ 2-we make a rank array who maintain the rank of node.all are ashgin as 0.
+ 
+ 
+     
+*/	
+	
+  public static void makeset(int v,int parent[],int rank[]) {
+	  for(int i=1;i<=v;i++) {
+		 parent[i]=i;
+		 rank[i]=0;
+	  }
+  }
+  
+  public static int findparent(int node,int parent[],int rank[]) {
+	  if(node==parent[node]) {
+		  return node;
+	  }
+	  
+	  return parent[node]=findparent(node, parent, rank);
+  }
+  
+  void union(int u,int v,int parent[],int rank[]) {
+	  u=findparent(u, null, null);
+	  v=findparent(v, null, null);
+	  if(rank[u]<rank[v]) {
+		  parent[u]=v;
+	  }else if(rank[v]<rank[u]) {
+		  parent[v]=u;
+	  }else {
+		  parent[v]=u;
+		  rank[u]++;
+	  }
+			  
+  }
+	
+	public static void main(String args[]) {
+		int parent[]=new int[1000];
+		int v=5;
+		int rank[]=new int[5]; 
+		
+		makeset(v,parent,rank);
+	}
 }
--- DSA__/src/graph/Graph16_Krukals_Algo_for_MST.java
+++ DSA__/src/graph/Graph16_Krukals_Algo_for_MST.java
@@ -1,5 +1,104 @@
 package graph;
 
-public class Graph16_Krukals_Algo_for_MST {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+class pair{
+	int u;
+	int v;
+	int weight;
+	
+	
+	pair(int u,int v,int weight){
+		this.v=v;
+		this.u=u;
+		this.weight=weight;
+	}
+	int getv() {
+		return v;
+	}
+	int getu() {
+		return u;
+	}
+	int getw() {
+		return weight;
+	}
+}
+class Sortcomprator implements Comparator<pair>{
+	
+	@Override
+	public int compare(pair o1, pair o2) {
+	if(o1.getw()<o2.getw()) {
+		return -1;
+	}
+	if(o1.getw()<o2.getw()) {
+		return 1;
+	}
+	
+	return 0;
+}
+}
 
+public class Graph16_Krukals_Algo_for_MST {
+public static int findPar(int u,int parent[]) {
+		  if(u==parent[u]) return u;
+			return parent[u] = findPar(parent[u], parent); 
+}
+	  
+public static void union(int u, int v, int parent[], int rank[]) {
+			u = findPar(u, parent); 
+			v = findPar(v, parent);
+			if(rank[u] < rank[v]) {
+	        	parent[u] = v;
+	        }
+	        else if(rank[v] < rank[u]) {
+	        	parent[v] = u; 
+	        }
+	        else {
+	        	parent[v] = u;
+	        	rank[u]++; 
+	        }
+		}
+public static void kurskals(ArrayList<pair> adj,int v) {
+	Collections.sort(adj, new Sortcomprator());
+	int parent[]=new int[v];
+	
+	int rank[]=new int[v]; 
+	
+	 for(int i=0;i<v;i++) {
+		 parent[i]=i;
+		 rank[i]=0;
+	  }
+	 
+	 int costMst=0;
+	 
+	 ArrayList<pair> mst=new ArrayList<>();
+	 for(pair it:adj) {
+		 if(findPar(it.getu(), parent)!= findPar(it.getv(), parent)) {
+			 costMst += it.getw(); 
+     		mst.add(it); 
+     		union(it.getu(), it.getv(), parent, rank);
+		 }
+	 }
+	 
+	 System.out.println(costMst);
+     for(pair it: mst) {
+     	System.out.println(it.getu() + " - " +it.getv()); 
+     }
+}
+public static void main(String args[]) {
+	int n = 5;
+    ArrayList<pair> adj = new ArrayList<pair>();
+	
+		
+	adj.add(new pair(0, 1, 2));
+	adj.add(new pair(0, 3, 6));
+	adj.add(new pair(1, 3, 8));
+	adj.add(new pair(1, 2, 3));
+	adj.add(new pair(1, 4, 5));
+	adj.add(new pair(2, 4, 7));
+	
+	kurskals(adj, n);
+}
+	
 }
