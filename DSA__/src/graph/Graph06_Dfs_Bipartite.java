6e6e9295f2b6edfae76117a7ae2da2046a573708
Mayank mishra <90859312+Mayank1242@users.noreply.github.com>
2022-06-21 07:19:23 -0700

    commit

--- DSA__/src/graph/Graph06_Dfs_Bipartite.java
+++ DSA__/src/graph/Graph06_Dfs_Bipartite.java
@@ -5,82 +5,93 @@ import java.util.Scanner;
 
 public class Graph06_Dfs_Bipartite {
 	
-public static boolean check_Bipartie(int node, ArrayList<ArrayList<Integer>> adj,int colour[]) {
-	
-	if(colour[node]==-1) {
-		colour[node]=1;
+	static boolean dfsCheck(ArrayList < ArrayList < Integer >> graph, int node, int color[]) {
+        for (Integer it: graph.get(node)) {
+            if (color[it] == -1) {
+                //Color of variable neighbor is the inverted color of variable node
+                color[it] = 1 - color[node];
 
-	}
-	
-	for(Integer it:adj.get(node)) {
-		if(colour[it]==-1) {
-			colour[it]=1-colour[node];
-			if(check_Bipartie(it, adj, colour)) {
-				return true;
-			}
-		}else if(colour[it]!=colour[node]) {
-			return true;
-		}
-	}
-	
-	
-	
-	
-	return false;
-}
+                if (!dfsCheck(graph, it, color))
+                    return false;
+            } else if (color[it] == color[node]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-public static boolean check_colour(ArrayList<ArrayList<Integer>> adj,int v) {
-	int colour[]=new int[v+1];
-	for(int i=0;i<v;i++) {
-		colour[i]=-1;
-	}
-	for(int i=0;i<=v;i++) {
-		if(colour[i]==-1) {
-			if(!check_Bipartie(i,adj,colour)) {
-				return false;
-			}
-		}
-	}
-	return true;
-}
-	
-public static void addedgege( ArrayList<ArrayList<Integer>> adj,int sourse,int dest) {
-		adj.get(sourse).add(dest);
-		adj.get(dest).add(sourse);
-		
-}
+    static boolean checkBipartite(ArrayList < ArrayList < Integer >> graph, int n) {
+        int color[] = new int[n];
 
-public static void main(String args[]) {
-//	1 2
-//	2 3
-//	2 7 
-//	7 6
-//	3 4
-//	6 5
-//	4 5
-//	5 8
-		int v=3;
-		int e=3;
-		Scanner sc=new Scanner(System.in);
-		ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-		
-		for(int i=1;i<=v+1;i++) {
-			adj.add(new ArrayList<Integer>());
-		}
-		
-		
-		for(int i=1;i<e;i++) {
-			int sourse=sc.nextInt();
-			int destination=sc.nextInt();
-			addedgege(adj,sourse,destination);
-	    }
+        for (int i = 0; i < n; i++) {
+            color[i] = -1;
+        }
 
-		
-		if(check_colour(adj, v)) {
-			System.out.println("Yes bipirate");
-		}else {
-			System.out.println("No bipirate");
-		}
-		
-}
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!dfsCheck(graph, i, color)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        int n = 6;
+        ArrayList < ArrayList < Integer >> graph = new ArrayList < ArrayList < Integer >> ();
+
+        for (int i = 0; i < n; i++)
+            graph.add(new ArrayList < Integer > ());
+
+        graph.get(0).add(1);
+        graph.get(1).add(0);
+
+        graph.get(1).add(2);
+        graph.get(2).add(1);
+
+        graph.get(1).add(4);
+        graph.get(4).add(1);
+
+        graph.get(1).add(5);
+        graph.get(5).add(1);
+
+        graph.get(2).add(3);
+        graph.get(3).add(2);
+
+        graph.get(3).add(4);
+        graph.get(4).add(3);
+
+        graph.get(3).add(5);
+        graph.get(5).add(3);
+
+        if (checkBipartite(graph, n))
+            System.out.println("It is a Bipartite Graph");
+        else
+            System.out.println("It is not a Bipartite Graph");
+    }
+//		int v=3;
+//		int e=3;
+//		Scanner sc=new Scanner(System.in);
+//		ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+//		
+//		for(int i=1;i<=v+1;i++) {
+//			adj.add(new ArrayList<Integer>());
+//		}
+//		
+//		
+//		for(int i=1;i<e;i++) {
+//			int sourse=sc.nextInt();
+//			int destination=sc.nextInt();
+//			addedgege(adj,sourse,destination);
+//	    }
+//
+//		
+//		if(check_colour(adj, v)) {
+//			System.out.println("Yes bipirate");
+//		}else {
+//			System.out.println("No bipirate");
+//		}
+//		
+//}
 }
--- DSA__/src/graph/Graph17_Bellman_Ford_Shortest_Path.java
+++ DSA__/src/graph/Graph17_Bellman_Ford_Shortest_Path.java
@@ -1,75 +1,68 @@
-//package graph;
-//import java.util.*;
-//class Node{
-//	int v;
-//	int weight;
-//	
-//	Node(int vi,int w){
-//		v=vi;
-//		weight=w;
-//	}
-//	Node(){
-//		
-//	}
-//	
-//	int getv() {
-//		return v;
-//		
-//	}
-//	int getw(){
-//		return weight;
-//	}
-//}
+package graph;
+import java.util.*;
+class Node 
+{
+	private int u;
+    private int v;
+    private int weight;
+    
+    Node(int _u, int _v, int _w) { u = _u; v = _v; weight = _w; }
+    
+    Node() {}
+    
+    int getV() { return v; }
+    int getU() { return u; }
+    int getWeight() { return weight; }
+
+}
 //
-//public class Graph17_Bellman_Ford_Shortest_Path {
-//public static void bellmenford(ArrayList<Node> adj,int n,int s) {
-//	int dest[]=new int[n];
-//	for(int i=0;i<n;i++) {
-//		dest[i]=Integer.MAX_VALUE;
-//		
-//	}
-//	dest[s]=0;
-//	
-//	for(int i=0;i<n-1;i++) {
-//		for(Node node:adj) {
-//			if(dest[node.getw()]+node.getw()<dest[node.getv()]) {
-//				dest[node.getv()]=dest[node.getw()]+node.getw();
-//			}
-//		}
-//	}
-//	
-//	int f=0;
-//	for(Node node:adj) {
-//		if(dest[node.getw()]+node.getw()<dest[node.getv()]) {
-//			dest[node.getv()]=dest[node.getw()]+node.getw();
-//		}
-//	}
-//	
-//	if(f==0) {
-//		System.out.println("Shortest path from " +s+" to destination is "+ dest[5] );
-//	}
-//	
-//}
-//public static void addedege(ArrayList<Node> adj,int v,int u,int w){
-//    ((List<Node>) adj.get(v)).add(new Node(u,w));
-////    adj.get(u).add(new Node(v,w));
-//}
-//public static void main(String args[]) {
-//	int e=7;
-//    int v=5;
-//	Scanner sc=new Scanner(System.in);
-//	ArrayList<Node> adj=new ArrayList<>();
-//	
-//	
-//	
-//	for(int i=0;i<e;i++){
-//	    int a=sc.nextInt();
-//	    int b=sc.nextInt();
-//	    int weight=sc.nextInt();
-//	    
-//	    addedege(adj,a,b,weight);
-//	    
-//	}
-//	bellmenford(adj, e, v);
-//}
-//}
+public class Graph17_Bellman_Ford_Shortest_Path {
+public static void bellmanFord(ArrayList<Node> edges, int N, int src)
+{
+    int dist[] = new int[N];
+    for(int i = 0;i<N;i++) dist[i] = 10000000; 
+
+    dist[src] = 0;
+
+	for(int i = 1;i<=N-1;i++) {
+		for(Node node : edges) {
+			if(dist[node.getU()] + node.getWeight() < dist[node.getV()]) {
+				dist[node.getV()] = dist[node.getU()] + node.getWeight(); 
+			}
+		}
+	}
+
+	int fl = 0; 
+	for(Node node: edges) {
+		if(dist[node.getU()] + node.getWeight() < dist[node.getV()]) {
+			fl = 1;
+			System.out.println("Negative Cycle"); 
+			break;
+		}
+	}
+
+	if(fl == 0) {
+		for(int i = 0;i<N;i++) {
+			System.out.print(  dist[i]+" "); 
+		}
+	}
+}
+public static void main(String args[])
+{
+    int n = 6;
+    ArrayList<Node> adj = new ArrayList<Node>();
+	
+		
+	adj.add(new Node(3, 2, 6));
+	adj.add(new Node(5, 3, 1));
+	adj.add(new Node(0, 1, 5));
+	adj.add(new Node(1, 5, -3));
+	adj.add(new Node(1, 2, -2));
+	adj.add(new Node(3, 4, -2));
+	adj.add(new Node(2, 4, 3));
+
+
+	bellmanFord(adj, n, 0);
+	
+}
+}
